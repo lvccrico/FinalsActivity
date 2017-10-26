48285320a69a026972b8ecdbcff102b4290fa385
Rico Custodio <rico.custodio@student.laverdad.edu.ph>
2017-10-26 10:09:45 +0800

    add user login/registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,8 +48,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
             'email' => 'required|string|email|max:255|unique:users',
+            'address' => 'required|string|max:200',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -63,8 +65,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
+            'address' => $data['address'],
             'password' => bcrypt($data['password']),
         ]);
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name', 20);
+            $table->string('last_name', 20); 
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address', 200);
             $table->rememberToken();
             $table->timestamps();
         });
